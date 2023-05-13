49e027fe36f54f02acb552b67284766b1e840341
carrascoArles <arlesc789@gmail.com>
2023-05-12 20:24:08 -0500

    2do commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,8 @@ public class HolaMundo{
     
 	 public static void main(String args[]){
 	 
-	  System.out.println("Hola mundo")
+	  System.out.println("Hola mundo");
+	  System.out.println("Bienvenida al curso");
 	 
 	 }
 
